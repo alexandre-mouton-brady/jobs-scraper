4fbb48b4e9502ea87dcaa46b87cdf43e16e9e999
Alexandre <amoutonbrady@gmail.com>
2018-11-03 19:56:04 +0100

    I'm no sure

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cors({ origin: process.env.CORS || '*' }));
   app.useStaticAssets(join(__dirname, '..', 'assets'), { prefix: '/imgs/' });
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 
 bootstrap();
